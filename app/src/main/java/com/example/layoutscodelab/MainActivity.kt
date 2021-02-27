8cbca7ee90bbce610a79c3dcc04c1ea88658f502
Hendrick <sebastinesoacatp@gmail.com>
2021-02-27 14:55:57 +0100

    Working with Lists

--- app/src/main/java/com/example/layoutscodelab/MainActivity.kt
+++ app/src/main/java/com/example/layoutscodelab/MainActivity.kt
@@ -9,8 +9,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -52,7 +54,20 @@ fun LayoutsCodeLab(){
         }
     ) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
-            BodyContent(Modifier.padding(innerPadding))
+            BodyContent(Modifier.padding(innerPadding).padding(8.dp))
+        }
+    }
+}
+
+@Composable
+fun SimpleList(){
+    // We save the scrolling position with this state that can always
+    // be used to programmatically scroll the list
+    val scrollState = rememberScrollState()
+
+    Column(Modifier.verticalScroll(scrollState)){
+        repeat(100){
+            Text("Item #$it")
         }
     }
 }
