2f6cf566a45f2f6bc08bb733d43c0d68432524c9
Hendrick <sebastinesoacatp@gmail.com>
2021-02-27 17:36:21 +0100

    Modifications with the list, making use of coroutines

--- app/src/main/java/com/example/layoutscodelab/MainActivity.kt
+++ app/src/main/java/com/example/layoutscodelab/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -25,6 +26,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.layoutscodelab.ui.theme.LayoutsCodeLabTheme
 import dev.chrisbanes.accompanist.coil.CoilImage
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,13 +66,37 @@ fun LayoutsCodeLab(){
 
 @Composable
 fun LazyList(){
-    // We save the scrolling position with this state that can also
-    // be used to programmatically scroll the list
+    val listSize = 100
+    // We save the scrolling position with this state
     val scrollState = rememberLazyListState()
+    // We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
 
-    LazyColumn(state = scrollState){
-        items(100){
-            ImageListItem(index = it)
+
+    Column {
+        Row{
+            Button(onClick = {
+                coroutineScope.launch {
+                    // 0 is the first item index
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text("Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    // listSize -1 is the last index of the list
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }){
+                Text("Scroll to the end")
+            }
+        }
+        LazyColumn(state = scrollState) {
+            items(100) {
+                ImageListItem(index = it)
+            }
         }
     }
 }
@@ -103,8 +129,6 @@ fun ImageListItem(index: Int){
 @Composable
 fun BodyContent(modifier: Modifier = Modifier){
     Column(modifier = modifier){
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
         LazyList()
     }
 }
